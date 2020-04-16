c0fb8f261c60f9513525701660c8290e0cce7b5c
PRAFULKHAIRE <prafulkhaire20@gmail.com>
2020-04-16 17:15:22 +0530

    Commite Backend Features

---
+++ onlinebookstore/src/main/java/com/prafulkhaire/onlinebookstore/repository/BookCategoryRepository.java
@@ -0,0 +1,12 @@
+package com.prafulkhaire.onlinebookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.prafulkhaire.onlinebookstore.entity.BookCategory;
+
+
+@RepositoryRestResource(collectionResourceRel="bookCategory", path="book-category")
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
+
+}
---
+++ onlinebookstore/src/main/java/com/prafulkhaire/onlinebookstore/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.prafulkhaire.onlinebookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prafulkhaire.onlinebookstore.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book,Long>{
+}
