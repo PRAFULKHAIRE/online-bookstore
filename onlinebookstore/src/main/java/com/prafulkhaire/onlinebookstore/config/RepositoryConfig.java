0432887c412e23dddc6a972b2d78085c4f49eada
PRAFULKHAIRE <prafulkhaire20@gmail.com>
2020-04-18 20:57:14 +0530

    Feature 1 all chaages done.

--- onlinebookstore/src/main/java/com/prafulkhaire/onlinebookstore/config/RepositoryConfig.java
+++ onlinebookstore/src/main/java/com/prafulkhaire/onlinebookstore/config/RepositoryConfig.java
@@ -3,6 +3,7 @@ package com.prafulkhaire.onlinebookstore.config;
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.Type;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -12,7 +13,7 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer {
 	
-	
+	@Autowired
 	private EntityManager entityManager;
 	
 	@Override
--- onlinebookstore/src/main/java/com/prafulkhaire/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/prafulkhaire/onlinebookstore/repository/BookRepository.java
@@ -1,8 +1,10 @@
 package com.prafulkhaire.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.prafulkhaire.onlinebookstore.entity.Book;
 
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book,Long>{
 }
